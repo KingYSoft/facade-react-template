054a25c1a462423b4927eacd3e3cb750fbbe42b7
xiaohua <ytoforever@aliyun.com>
2020-03-25 13:35:49 +0800

    Change README.md

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 // import { createStore } from "redux";
 
-import store from './redux/reducers/index';
+import store from './redux/store';
 
 import './index.less';
 import App from './app';
--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,20 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// import createSagaMiddleware from "redux-saga";
-import { applyMiddleware, createStore, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
-// import myMiddleware from "./middleware";
+import { combineReducers } from 'redux';
+import counter from './counter';
 
-// 添加中间件和调试工具
-// const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers =
-  typeof window === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const middlewares = [thunk];
-
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
-// sagaMiddleware.run(mainSaga);
-
-export default store;
+export default combineReducers({ counter });
